280eee075e3800c3aed6e4b380f2cc14f6ad39e3
marto ndegz <martondegz@gmail.com>
2016-08-06 17:31:04 +0300

    deleted

---
+++ .idea/job_matcher.py
@@ -0,0 +1,10 @@
+"""
+Given 50 randomly located drivers, map them to some random number of randomly located jobs such that
+total distance to jobs is minimised. With 50 jobs available, map the driver ids and job location ids
+and print out the result in a key value pair
+"""
+
+#declare number of jobs in a range
+#declare number of drivers in a range
+
+
